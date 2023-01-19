34ba7df09b1e5610a35468469f181589970ad326
Artiom Nazarchuk <sel_it@tut.by>
2023-01-19 15:14:54 +0300

    Refactoring hashing, implementation of login by email and use of token through POSTMAN

--- auth/users/views.py
+++ auth/users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from .models import User
+from rest_framework.exceptions import AuthenticationFailed
+import jwt, datetime
 
 class RegisterView(APIView):
     def post(self, request):
@@ -9,3 +12,28 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('Пользователь не найден')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Неверный пароль')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=300),
+            'iat': datetime.datetime.utcnow(),
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256').decode("utf-8")
+
+        return Response({
+            'jwt': token
+        })
